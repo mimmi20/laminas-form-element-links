fb5cb29eb1ab1e6baa7f56e67bb6c8b59362f0fb
Thomas Müller <mimmi20@live.de>
2022-12-15 16:58:03 +0100

    update dependencies

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -13,7 +13,7 @@ declare(strict_types = 1);
 $header = <<<'EOF'
     This file is part of the mimmi20/laminas-form-element-links package.
 
-    Copyright (c) 2021, Thomas Mueller <mimmi20@live.de>
+    Copyright (c) 2021-2022, Thomas Mueller <mimmi20@live.de>
 
     For the full copyright and license information, please view the LICENSE
     file that was distributed with this source code.
